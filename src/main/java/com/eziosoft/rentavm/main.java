7f543649345e527cb40b826ec7d5f9ea87174873
Jonathan <jedijon567@gmail.com>
2021-04-20 13:29:14 -0400

    it works

--- src/main/java/com/eziosoft/rentavm/main.java
+++ src/main/java/com/eziosoft/rentavm/main.java
@@ -1,7 +1,31 @@
 package com.eziosoft.rentavm;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
 public class main {
-    public static void main(String[] args){
-        System.out.println("do epic things here");
+    public static void main(String[] args) throws IOException{
+        // start by making a httpserver instance
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 6969), 0);
+        server.createContext("/", new Page());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("web server started");
     }
+
+    static class Page implements HttpHandler{
+        @Override
+        public void handle(HttpExchange t) throws IOException{
+            String what = "cool";
+            t.sendResponseHeaders(200, what.length());
+            t.getResponseBody().write(what.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+
 }
