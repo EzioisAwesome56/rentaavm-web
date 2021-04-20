7118376424006799e5e90ede866dbff9dc9ff616
Jonathan <jedijon567@gmail.com>
2021-04-20 14:26:10 -0400

    database crap

---
+++ src/main/java/com/eziosoft/rentavm/database.java
@@ -0,0 +1,37 @@
+package com.eziosoft.rentavm;
+
+import com.google.gson.Gson;
+import com.rethinkdb.net.Connection;
+import com.rethinkdb.RethinkDB;
+
+public class database {
+
+    private static Connection thonk;
+    private static final RethinkDB r = RethinkDB.r;
+    private static Gson gson = new Gson();
+
+    public static void DatabaseInit(){
+        System.out.println("Eziosoft Rentavm database driver starting up...");
+        // build connection
+        Connection.Builder builder = r.connection().hostname("h").port(28015);
+        thonk = builder.connect();
+
+        // basically lifted from Dankcord: check to see if our database actually friggin exists
+        System.out.println("Checking if database exists...");
+        if (!r.dbList().contains("rentavm").run(thonk, Boolean.class).first()){
+            System.out.println("Database does not exist! creating it...");
+            r.dbCreate("rentavm").run(thonk);
+            r.db("rentavm").tableCreate("session").optArg("primary_key", "token").run(thonk);
+            r.db("rentavm").tableCreate("users").optArg("primary_key", "username").run(thonk);
+            // some other third thing we might store in the database later idk
+            //r.db("rentavm").tableCreate("auth").optArg("primary_key", "username").run(thonk);
+            System.out.println("Database created!");
+        } else {
+            System.out.println("Database already exists!");
+        }
+        // bind our connection to it
+        thonk.use("rentavm");
+        // init done
+        System.out.println("Database driver has finished starting up!");
+    }
+}
--- src/main/java/com/eziosoft/rentavm/objects/session.java
+++ src/main/java/com/eziosoft/rentavm/objects/session.java
@@ -1,16 +1,16 @@
 package com.eziosoft.rentavm.objects;
 
 public class session {
-    private String session;
+    private String token;
     private boolean isActive;
 
     public session(String token){
-        this.session = token;
+        this.token = token;
         this.isActive = true;
     }
 
-    public String getSession() {
-        return session;
+    public String getToken() {
+        return token;
     }
 
     public boolean isActive() {
