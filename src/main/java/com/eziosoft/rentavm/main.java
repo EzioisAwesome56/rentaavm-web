df0f3ba8b5f3c92818db1530d03c724a4b91f880
Jonathan <jedijon567@gmail.com>
2021-04-20 13:41:18 -0400

    make loading html files from resources work

--- src/main/java/com/eziosoft/rentavm/main.java
+++ src/main/java/com/eziosoft/rentavm/main.java
@@ -6,24 +6,24 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws IOException{
         // start by making a httpserver instance
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 6969), 0);
-        server.createContext("/", new Page());
+        server.createContext("/", new landing());
         server.setExecutor(null);
         server.start();
         System.out.println("web server started");
     }
 
-    static class Page implements HttpHandler{
-        @Override
+    static class landing implements HttpHandler{
         public void handle(HttpExchange t) throws IOException{
-            String what = "cool";
+            String what = new Scanner(main.class.getResourceAsStream("/main.html"), "UTF-8").useDelimiter("\\A").next();
+            System.out.println(what);
             t.sendResponseHeaders(200, what.length());
-            t.getResponseBody().write(what.getBytes(StandardCharsets.UTF_8));
+            t.getResponseBody().write(what.getBytes());
         }
     }
 
